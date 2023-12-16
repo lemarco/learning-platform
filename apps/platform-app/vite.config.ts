d946874bb77e77e314043e6537cec4a86979bd77
lemarc <lemarc.dev@gmail.com>
2023-12-16 21:13:54 +0100

    add config reader variables in platform app

--- apps/platform-app/vite.config.ts
+++ apps/platform-app/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import dotenv from 'dotenv';
+import { readFileSync } from 'fs';
+const config = dotenv.parse(readFileSync('../../.env'));
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 10000,
-    host: '172.17.0.1',
+    port: +config.PLATFORM_APP_FRONTEND_PORT,
+    host: config.INNER_DOCKER_HOST,
   },
 });
