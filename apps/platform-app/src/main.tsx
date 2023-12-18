50b80ac3c3d00b7f5648f454b99048348bfeab71
Le Marc <lemarc.dev@gmail.com>
2023-12-18 20:46:49 +0000

    fix network issue in docker network

--- apps/platform-app/src/main.tsx
+++ apps/platform-app/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
+console.log('PLATFORM APP ENTRY POINT CHECK!!!!');
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
--- apps/platform-app/vite.config.ts
+++ apps/platform-app/vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react-swc';
 import dotenv from 'dotenv';
 import { readFileSync } from 'fs';
 const config = dotenv.parse(readFileSync('../../.env'));
-console.log('config = ', config);
+console.log('PLATFORM_APP_FRONTEND_PORT = ', config.PLATFORM_APP_FRONTEND_PORT);
 export default defineConfig({
   plugins: [react()],
   server: {
     port: +config.PLATFORM_APP_FRONTEND_PORT,
+    host: '0.0.0.0',
     // host: config.INNER_DOCKER_HOST,
   },
 });
