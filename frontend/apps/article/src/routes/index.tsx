1ab567d472929a54e8baeb4e0b7c3284d9bd9a09
Alexander Pavlyuchuk <apavlyuc@gmail.com>
2024-01-04 19:25:00 +0100

    remove three-dots-menu from articles page (#27)

--- frontend/apps/article/src/routes/index.tsx
+++ frontend/apps/article/src/routes/index.tsx
@@ -695,40 +695,6 @@ const SocialMediaShare = component$(() => {
           Save this article
           <div class="tooltip-arrow" data-popper-arrow />
         </div>
-        <button
-          id="dropdownMenuIconHorizontalButton"
-          data-dropdown-toggle="dropdownDotsHorizontal"
-          class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-500 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          type="button"
-        >
-          <svg
-            class="w-5 h-5 text-gray-500 dark:text-gray-400"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 16 3"
-          >
-            <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
-          </svg>
-        </button>
-
-        <div
-          id="dropdownDotsHorizontal"
-          class="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
-        >
-          <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconHorizontalButton">
-            <li>
-              <a href="/" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                Add to collection
-              </a>
-            </li>
-            <li>
-              <a href="/" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                Report
-              </a>
-            </li>
-          </ul>
-        </div>
       </div>
     </aside>
   );
