b738252833b6eba84993b67b63976f3e015c8677
Alexander Pavlyuchuk <apavlyuc@gmail.com>
2024-01-05 09:52:27 +0100

    create RelatedArticle object with generic properties & use it dynamically during html creation for articles pages (#28)

--- frontend/apps/article/src/routes/index.tsx
+++ frontend/apps/article/src/routes/index.tsx
@@ -1,88 +1,70 @@
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 import { component$ } from "@builder.io/qwik";
+
+type RelatedArticleInfo = { image: string; link: string; title: string; previewText: string; estimatedReadTime: number };
+
+const RelatedArticle = component$(({ image, link, title, previewText, estimatedReadTime }: RelatedArticleInfo) => {
+  return (
+    <article class="flex flex-col xl:flex-row">
+      <a href="/" class="mb-2 xl:mb-0">
+        <img src={image} class="mr-5 max-w-sm" alt="1 " />
+      </a>
+      <div class="flex flex-col justify-center">
+        <h2 class="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
+          <a href={link}>{title}</a>
+        </h2>
+        <p class="mb-4 text-gray-500 dark:text-gray-400 max-w-sm">{previewText}</p>
+        <a
+          href={link}
+          class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline"
+        >
+          Read in {estimatedReadTime} minute{estimatedReadTime === 1 ? "" : "s"}
+        </a>
+      </div>
+    </article>
+  );
+});
+
 const RelatedArticles = component$(() => {
+  const articleInfos: RelatedArticleInfo[] = [
+    {
+      image: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-2.png",
+      link: "/blog/1",
+      title: "Enterprise design tips",
+      previewText: "Over the past year, Volosoft has undergone many changes! After months of preparation.",
+      estimatedReadTime: 12,
+    },
+    {
+      image: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-2.png",
+      link: "/blog/2",
+      title: "Enterprise design tips",
+      previewText: "Over the past year, Volosoft has undergone many changes! After months of preparation.",
+      estimatedReadTime: 12,
+    },
+    {
+      image: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-3.png",
+      link: "/blog/3",
+      title: "We partnered up with Google",
+      previewText: "Over the past year, Volosoft has undergone many changes! After months of preparation.",
+      estimatedReadTime: 8,
+    },
+    {
+      image: "https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-4.png",
+      link: "/blog/4",
+      title: "Our first project with React",
+      previewText: "Over the past year, Volosoft has undergone many changes! After months of preparation.",
+      estimatedReadTime: 1,
+    },
+  ];
   return (
     <aside aria-label="Related articles" class="py-8 lg:py-24 bg-white dark:bg-gray-900">
       <div class="px-4 mx-auto max-w-screen-xl">
         <h2 class="mb-6 lg:mb-8 text-2xl font-bold text-gray-900 dark:text-white">Related articles</h2>
         <div class="grid gap-6 lg:gap-12 md:grid-cols-2">
-          <article class="flex flex-col xl:flex-row">
-            <a href="/" class="mb-2 xl:mb-0">
-              <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-1.png" class="mr-5 max-w-sm" alt="1 " />
-            </a>
-            <div class="flex flex-col justify-center">
-              <h2 class="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
-                <a href="/">Our first office</a>
-              </h2>
-              <p class="mb-4 text-gray-500 dark:text-gray-400 max-w-sm">
-                Over the past year, Volosoft has undergone many changes! After months of preparation.
-              </p>
-              <a
-                href="/"
-                class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline"
-              >
-                Read in 2 minutes
-              </a>
-            </div>
-          </article>
-          <article class="flex flex-col xl:flex-row">
-            <a href="/" class="mb-2 xl:mb-0">
-              <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-2.png" class="mr-5 max-w-sm" alt=" 2" />
-            </a>
-            <div class="flex flex-col justify-center">
-              <h2 class="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
-                <a href="/">Enterprise design tips</a>
-              </h2>
-              <p class="mb-4 text-gray-500 dark:text-gray-400 max-w-sm">
-                Over the past year, Volosoft has undergone many changes! After months of preparation.
-              </p>
-              <a
-                href="/"
-                class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline"
-              >
-                Read in 12 minutes
-              </a>
-            </div>
-          </article>
-          <article class="flex flex-col xl:flex-row">
-            <a href="/" class="mb-2 xl:mb-0">
-              <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-3.png" class="mr-5 max-w-sm" alt=" 3" />
-            </a>
-            <div class="flex flex-col justify-center">
-              <h2 class="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
-                <a href="/">We partnered up with Google</a>
-              </h2>
-              <p class="mb-4 text-gray-500 dark:text-gray-400 max-w-sm">
-                Over the past year, Volosoft has undergone many changes! After months of preparation.
-              </p>
-              <a
-                href="/"
-                class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline"
-              >
-                Read in 8 minutes
-              </a>
-            </div>
-          </article>
-          <article class="flex flex-col xl:flex-row">
-            <a href="/" class="mb-2 xl:mb-0">
-              <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/article/blog-4.png" class="mr-5 max-w-sm" alt=" 4" />
-            </a>
-            <div class="flex flex-col justify-center">
-              <h2 class="mb-2 text-xl font-bold leading-tight text-gray-900 dark:text-white">
-                <a href="/">Our first project with React</a>
-              </h2>
-              <p class="mb-4 text-gray-500 dark:text-gray-400 max-w-sm">
-                Over the past year, Volosoft has undergone many changes! After months of preparation.
-              </p>
-              <a
-                href="/"
-                class="inline-flex items-center font-medium underline underline-offset-4 text-primary-600 dark:text-primary-500 hover:no-underline"
-              >
-                Read in 12 minutes
-              </a>
-            </div>
-          </article>
+          {articleInfos.map((article, index) => (
+            <RelatedArticle {...article} key={index} />
+          ))}
         </div>
       </div>
     </aside>
