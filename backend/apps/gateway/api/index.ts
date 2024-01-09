8c1eb4844d3e2ae8add48f1f02d02f37987143ff
Le Marc <lemarc.dev@gmail.com>
2024-01-09 15:23:52 +0000

    article api definition in gateway

--- backend/apps/gateway/api/index.ts
+++ backend/apps/gateway/api/index.ts
@@ -1,4 +1,21 @@
 import Elysia, { Context, Handler } from "elysia";
-import { App } from "..";
+import { App, servicesBaseUrls } from "..";
 
-export const ApiRoutesHandler = (app: App) => app.group("/api", (app) => app.get("/", (ctx) => new Response()));
+const ArticlesGroupHandler = (app: App) => {
+  return app.group("/articles", (app) => {
+    return app
+      .get("/", ({ query }) => fetch(`${servicesBaseUrls.authQuery}?limit=${query.limit}&page=${query.page}`))
+      .get("/:id", ({ query }) => fetch(`${servicesBaseUrls.authQuery}${query.id}`))
+      .get("/related", () => fetch(`${servicesBaseUrls.authQuery}related`))
+      .get("/latest", () => fetch(`${servicesBaseUrls.authQuery}latest`))
+      .get("/tags/recomeded", () => fetch(`${servicesBaseUrls.authQuery}/tags/recommended`));
+  });
+};
+
+const ProfileGroupHandler = (app: App) => {
+  return app.group("/profile", (app) => {
+    return app.get("/:id", ({ query }) => fetch(`${servicesBaseUrls.usersQuery}${query.id}`));
+  });
+};
+
+export const ApiRoutesHandler = (app: App) => app.group("/api", (app) => app.use(ArticlesGroupHandler).use(ProfileGroupHandler));
