6b4f1d142eb3bf10b2f093779ab5c575dcdc69c8
Le Marc <lemarc.dev@gmail.com>
2024-01-18 23:04:26 +0000

    remove styling config lib

--- frontend/libs/styling-config/src/lib/styling-config.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { createDOM } from "@builder.io/qwik/testing";
-import { expect, test } from "vitest";
-import { StylingConfig } from "./styling-config";
-
-test(`[StylingConfig Component]: Should render`, async () => {
-  const { screen, render } = await createDOM();
-  await render(<StylingConfig />);
-  expect(screen.innerHTML).toContain("StylingConfig works!");
-});
