69ab2ed038897776a430ebf74d3c35c633956080
Le Marc <lemarc.dev@gmail.com>
2024-01-18 23:47:03 +0000

    add error overlay and add editor component

--- frontend/apps/editor/src/root.tsx
+++ frontend/apps/editor/src/root.tsx
@@ -1,21 +1,22 @@
-import { component$ } from "@builder.io/qwik";
-import {
-  QwikCityProvider,
-  RouterOutlet,
-  ServiceWorkerRegister,
-} from "@builder.io/qwik-city";
+import { component$, useOnDocument, $, useStore, useOn, useOnWindow, useStyles$ } from "@builder.io/qwik";
+import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
-
-import "./global.css";
-
+import globalStyles from "./global.css?inline";
+const overlay = (err: Event) => {
+  const ErrorOverlay = customElements.get("vite-error-overlay");
+  if (!ErrorOverlay) {
+    return;
+  }
+  const overlay = new ErrorOverlay(err);
+  const body = document.body;
+  if (body !== null) {
+    body.appendChild(overlay);
+  }
+};
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Don't remove the `<head>` and `<body>` elements.
-   */
-
+  useStyles$(globalStyles);
+  useOnWindow("error", $(overlay));
+  useOnWindow("unhandledrejection", $(overlay));
   return (
     <QwikCityProvider>
       <head>
--- frontend/apps/editor/src/routes/index.tsx
+++ frontend/apps/editor/src/routes/index.tsx
@@ -1,16 +1,36 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useContext, useStore, createContextId, useContextProvider } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { Editor } from "../components/editor";
 
+export const LexicalComposerContext = createContextId<{ items: string[] }>("LexicalComposerContext");
+export const TableContext = createContextId<{ items: string[] }>("TableContext");
 export default component$(() => {
+  const initialConfig = {
+    editorState: undefined,
+    namespace: "Editor",
+    // nodes: [...PlaygroundNodes],
+    onError: (error: Error) => {
+      throw error;
+    },
+    // theme: PlaygroundEditorTheme,
+  };
+  useContextProvider(
+    LexicalComposerContext,
+    useStore({
+      items: ["Learn Qwik", "Build Qwik app", "Profit"],
+    }),
+  );
+  useContextProvider(
+    TableContext,
+    useStore({
+      items: ["Learn Qwik", "Build Qwik app", "Profit"],
+    }),
+  );
+
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
+    <div class="editor-shell">
+      <Editor />
+    </div>
   );
 });
 
@@ -23,3 +43,37 @@ export const head: DocumentHead = {
     },
   ],
 };
+
+// import { LexicalComposer } from "@lexical/react/LexicalComposer";
+// import Editor from "./Editor";
+// import { SharedAutocompleteContext } from "./context/SharedAutocompleteContext";
+// import { SharedHistoryContext } from "./context/SharedHistoryContext";
+// import PlaygroundNodes from "./nodes/PlaygroundNodes";
+// import { TableContext } from "./plugins/TablePlugin";
+// import PlaygroundEditorTheme from "./themes/PlaygroundEditorTheme";
+
+// function App(): JSX.Element {
+//   const initialConfig = {
+//     editorState: undefined,
+//     namespace: "Playground",
+//     nodes: [...PlaygroundNodes],
+//     onError: (error: Error) => {
+//       throw error;
+//     },
+//     theme: PlaygroundEditorTheme,
+//   };
+
+//   return (
+//     <LexicalComposer initialConfig={initialConfig}>
+//       <SharedHistoryContext>
+//         <TableContext>
+//           <SharedAutocompleteContext>
+//             <div className="editor-shell">
+//               <Editor />
+//             </div>
+//           </SharedAutocompleteContext>
+//         </TableContext>
+//       </SharedHistoryContext>
+//     </LexicalComposer>
+//   );
+// }
