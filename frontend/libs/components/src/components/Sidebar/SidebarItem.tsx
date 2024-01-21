47eb33eb7f8317a0a04da391f15222f31784275b
Le Marc <lemarc.dev@gmail.com>
2024-01-21 15:12:29 +0000

    Sidebar icon

--- frontend/libs/components/src/components/Sidebar/SidebarItem.tsx
+++ frontend/libs/components/src/components/Sidebar/SidebarItem.tsx
@@ -7,6 +7,7 @@ import { Tooltip } from "../Tooltip";
 import { SidebarContext } from "./SidebarContext";
 import { Slot, component$, useContext } from "@builder.io/qwik";
 import { SidebarItemContext } from "./SidebarItemContext";
+import { Icon, IconName } from "../icons";
 // import { useSidebarContext } from "./SidebarContext";
 // import { useSidebarItemContext } from "./SidebarItemContext";
 
@@ -33,7 +34,7 @@ export interface SidebarItemProps {
   as?: Element;
   href?: string;
   className?: string;
-
+  iconName?: IconName;
   label?: string;
   labelColor?: keyof SidebarItemLabelColors;
   theme?: DeepPartial<FlowbiteSidebarItemTheme>;
@@ -85,19 +86,21 @@ export const SidebarItem = component$<SidebarItemProps>(
     label,
     labelColor = "info",
     theme: customTheme = {},
+    iconName,
     ...props
   }) => {
-    const id = useId();
+    const id = Math.random().toString();
     const { theme: rootTheme, isCollapsed } = useContext(SidebarContext);
     const { isInsideCollapse } = useContext(SidebarItemContext);
 
     const theme = mergeDeep(rootTheme.item, customTheme);
 
     return (
-      <ListItem theme={theme} class={theme.listItem} id={id} isCollapsed={isCollapsed} tooltipChildren={children}>
+      <ListItem theme={theme} className={theme.listItem} id={id} isCollapsed={isCollapsed}>
+        <Slot q:slot="tooltip" name="tooltip" />
+
         <a
-          aria-labelledby={`flowbite-sidebar-item-${id}`}
-          ref={ref}
+          // ref={ref}
           class={twMerge(
             theme.base,
             isActive && theme.active,
@@ -106,11 +109,9 @@ export const SidebarItem = component$<SidebarItemProps>(
           )}
           {...props}
         >
-          {Icon && (
-            <Icon aria-hidden data-testid="flowbite-sidebar-item-icon" class={twMerge(theme.icon?.base, isActive && theme.icon?.active)} />
-          )}
+          {iconName && <Icon name={iconName} className={twMerge(theme.icon?.base, isActive && theme.icon?.active)} />}
           <Slot q:slot="icon" />
-          {isCollapsed && !Icon && <span class={theme.collapsed?.noIcon}>{(children as string).charAt(0).toLocaleUpperCase() ?? "?"}</span>}
+          {/* {isCollapsed && !Icon && <span class={theme.collapsed?.noIcon}>{(children as string).charAt(0).toLocaleUpperCase() ?? "?"}</span>} */}
           {!isCollapsed && (
             <Children id={id} theme={theme}>
               <Slot />
