90c3c0945f02c1185144a16e44b405b526b7c03f
Le Marc <lemarc.dev@gmail.com>
2024-01-28 12:46:36 +0000

    loading page test. add spinner

--- frontend/apps/main/app/[language]/(platform)/editor/page.tsx
+++ frontend/apps/main/app/[language]/(platform)/editor/page.tsx
@@ -1,5 +1,8 @@
 import { getMetadataGenerator } from "@/services/i18n";
+import { sleep } from "@/utils/sleep";
+
 export const generateMetadata = getMetadataGenerator("editor");
-export default function Editor() {
+export default async function Editor() {
+  await sleep(10000); // testing loading page
   return <>Editor page</>;
 }
