d793e24f95447acd579bc6608475a7ff2beb330f
Le Marc <lemarc.dev@gmail.com>
2024-01-28 10:32:18 +0000

    add root folder structure in router

---
+++ frontend/apps/main/app/[language]/(blog)/layout.tsx
@@ -0,0 +1,3 @@
+export default function BlogLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
---
+++ frontend/apps/main/app/[language]/(platform)/layout.tsx
@@ -0,0 +1,3 @@
+export default function PlatformLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
